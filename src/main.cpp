3e4ea7a7a494ced71bcca29ef5521bdfb79e666d
unknown <jamesm8079@gmail.com>
2024-02-01 16:10:07 +0100

    Version 14

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "myclass.cpp"
+
+int main()
+{
+    std::cout<<"Version 14"<<std::endl;
+    MyClass sample;
+    sample.message();
+    return 0;
+}
---
+++ src/myclass.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+class MyClass
+{
+    public:
+    void message()
+    {
+        std::cout<<"Sample message"<<std::endl;
+        std::cout<<"Version 14"<<std::endl;
+    }
+
+};
